8ada5cb0d9aefaea8b94f5173d0adc60f61461a2
Andy Brunner <andy@cloudxs.ch>
2021-03-18 13:14:57 +0100

    EFS added

--- lib/s3antivirus-stack.ts
+++ lib/s3antivirus-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as efs from '@aws-cdk/aws-efs';
 
 export class S3AntivirusStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,9 +18,32 @@ export class S3AntivirusStack extends cdk.Stack {
       ]
     });
 
+    // security groups
+    //const sg = new ec2.SecurityGroup(this, 'AvVPCSg',{
+    //  vpc: vpc
+    //});
+
+    // VPC endpoint
     const vpcep = new ec2.GatewayVpcEndpoint(this, 'AvVPCEp',{
       service: ec2.GatewayVpcEndpointAwsService.S3,
       vpc: vpc
     });
+
+    // EFS - Elastic File System
+    const fileSystem = new efs.FileSystem(this, 'AvEfs', {
+      vpc: vpc,
+      encrypted: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    fileSystem.addAccessPoint('AvEfsAp',{
+      path: '/lambda',
+      createAcl: {
+        ownerGid: "1000",
+        ownerUid: "1000",
+        permissions: "0777"
+      }
+    });
+
   }
 }
