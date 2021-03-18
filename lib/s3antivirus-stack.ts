6df9a721c8a42fb0014791a3d348bdb499c46b54
Andy Brunner <andy@cloudxs.ch>
2021-03-18 08:53:39 +0100

    vpc added

--- lib/s3antivirus-stack.ts
+++ lib/s3antivirus-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class S3AntivirusStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create network Resources
+    const vpc = new ec2.Vpc(this, 'AvVPC',{
+      cidr: "192.168.0.0/24",
+      subnetConfiguration: [
+        {
+          cidrMask: 25,
+          name: 'application',
+          subnetType: ec2.SubnetType.ISOLATED
+        }
+      ]
+    });
   }
 }
