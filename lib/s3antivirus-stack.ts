256433872165cc5510bf76eb40c41851f72ba6c6
Andy Brunner <andy@cloudxs.ch>
2021-03-19 12:09:14 +0100

    lambda not working

--- lib/s3antivirus-stack.ts
+++ lib/s3antivirus-stack.ts
@@ -3,7 +3,6 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as efs from '@aws-cdk/aws-efs';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as path from 'path';
-import { Duration } from '@aws-cdk/core';
 import { avConfig } from './av-config';
 
 export class S3AntivirusStack extends cdk.Stack {
@@ -11,7 +10,7 @@ export class S3AntivirusStack extends cdk.Stack {
     super(scope, id, props);
 
     // create network Resources
-    const vpc = new ec2.Vpc(this, 'AvVPC',{
+    const vpc = new ec2.Vpc(this, 'AvVPC', {
       cidr: "192.168.0.0/24",
       subnetConfiguration: [
         {
@@ -28,7 +27,7 @@ export class S3AntivirusStack extends cdk.Stack {
     //});
 
     // VPC endpoint
-    const vpcep = new ec2.GatewayVpcEndpoint(this, 'AvVPCEp',{
+    const vpcep = new ec2.GatewayVpcEndpoint(this, 'AvVPCEp', {
       service: ec2.GatewayVpcEndpointAwsService.S3,
       vpc: vpc
     });
@@ -40,12 +39,16 @@ export class S3AntivirusStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    const efsAp = fileSystem.addAccessPoint('AvEfsAp',{
+    const efsAp = fileSystem.addAccessPoint('AvEfsAp', {
       path: avConfig.workdir,
       createAcl: {
         ownerGid: "1000",
         ownerUid: "1000",
         permissions: "0777"
+      },
+      posixUser: {
+        gid: "1000",
+        uid: "1000"
       }
     });
 
@@ -55,9 +58,12 @@ export class S3AntivirusStack extends cdk.Stack {
       handler: 'download-definitions.lambdaHandleEvent',
       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
       vpc:vpc,
-      filesystem: lambda.FileSystem.fromEfsAccessPoint(efsAp, avConfig.mountpoint ),
+      filesystem: lambda.FileSystem.fromEfsAccessPoint(efsAp, avConfig.mountpoint),
       memorySize: 1024,
-      timeout: Duration.minutes(1)
+      timeout: cdk.Duration.minutes(1),
+      environment: {
+        'FRESHCLAM_WORK_DIR': `${avConfig.mountpoint}/`
+      }
     });
 
   }
