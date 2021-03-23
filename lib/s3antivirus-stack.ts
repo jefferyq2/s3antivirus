e42a6c2259f2608f8f31201eac90e1ab943bf150
Andy Brunner <andy@cloudxs.ch>
2021-03-23 07:51:01 +0100

    Def Download funktioniert

--- lib/s3antivirus-stack.ts
+++ lib/s3antivirus-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as efs from '@aws-cdk/aws-efs';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as path from 'path';
 import { avConfig } from './av-config';
@@ -9,17 +10,46 @@ export class S3AntivirusStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //#region - Download AV definitions from the web
+    // S3 bucket for AV definitions
+    const s3avdev = new s3.Bucket(this, 'definitions',{
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    // Lambda function that downloads definition files
+    const fnDefDl = new lambda.Function(this, 'definitionsDownload', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'download-definitions.lambdaHandleEvent',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      memorySize: 1024,
+      timeout: cdk.Duration.minutes(2),
+      environment: {
+        'CLAMAV_BUCKET_NAME': s3avdev.bucketName,
+        'PATH_TO_AV_DEFINITIONS': 'avdefinitions'
+      }
+    });
+
+    s3avdev.grantReadWrite(fnDefDl);
+    //#endregion
+
+
+
     // create network Resources
-    const vpc = new ec2.Vpc(this, 'AvVPC', {
+/*     const vpc = new ec2.Vpc(this, 'AvVPC', {
       cidr: "192.168.0.0/24",
       subnetConfiguration: [
         {
           cidrMask: 27,
           name: 'subnet',
-          subnetType: ec2.SubnetType.ISOLATED
+          subnetType: ec2.SubnetType.PUBLIC
         }
       ]
-    });
+    }); */
+
+/*     const vpc = ec2.Vpc.fromLookup(this, "defaultVPC",{
+      isDefault: true
+    }); */
 
     // security groups
     //const sg = new ec2.SecurityGroup(this, 'AvVPCSg',{
@@ -27,7 +57,7 @@ export class S3AntivirusStack extends cdk.Stack {
     //});
 
     // VPC endpoint
-    const vpcep = new ec2.GatewayVpcEndpoint(this, 'AvVPCEp', {
+/*     const vpcep = new ec2.GatewayVpcEndpoint(this, 'AvVPCEp', {
       service: ec2.GatewayVpcEndpointAwsService.S3,
       vpc: vpc
     });
@@ -39,6 +69,7 @@ export class S3AntivirusStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
+    // EFS - Accesspoint
     const efsAp = fileSystem.addAccessPoint('AvEfsAp', {
       path: avConfig.workdir,
       createAcl: {
@@ -50,21 +81,7 @@ export class S3AntivirusStack extends cdk.Stack {
         gid: "1000",
         uid: "1000"
       }
-    });
-
-    // Lambda function that downloads definition files
-    const fn = new lambda.Function(this, 'AvLmbdDl', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'download-definitions.lambdaHandleEvent',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
-      vpc:vpc,
-      filesystem: lambda.FileSystem.fromEfsAccessPoint(efsAp, avConfig.mountpoint),
-      memorySize: 1024,
-      timeout: cdk.Duration.minutes(1),
-      environment: {
-        'FRESHCLAM_WORK_DIR': `${avConfig.mountpoint}/`
-      }
-    });
+    }); */
 
   }
 }
