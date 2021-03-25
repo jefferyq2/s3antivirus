64aaeb0953b96902603d16913cbe8b47ab5fd210
Andy Brunner <andy@cloudxs.ch>
2021-03-25 12:05:47 +0100

    one lambda triggers the other not working

--- lib/s3antivirus-stack.ts
+++ lib/s3antivirus-stack.ts
@@ -3,54 +3,21 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as efs from '@aws-cdk/aws-efs';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as destinations from '@aws-cdk/aws-lambda-destinations';
 import * as event from '@aws-cdk/aws-events';
 import * as targets from '@aws-cdk/aws-events-targets';
 import * as path from 'path';
 import { avConfig } from './av-config';
 
 export class S3AntivirusStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor (scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // #region - Download AV definitions from the web
     // S3 bucket for AV definitions
     const s3avdev = new s3.Bucket(this, 'definitions', {
       encryption: s3.BucketEncryption.S3_MANAGED,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
-
-    // Lambda function that downloads definition files
-    const fnDefDl = new lambda.Function(this, 'definitionsDownload', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'download-definitions.lambdaHandleEvent',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda', 'clamav')),
-      memorySize: 1024,
-      timeout: cdk.Duration.minutes(2),
-      environment: {
-        CLAMAV_BUCKET_NAME: s3avdev.bucketName,
-        PATH_TO_AV_DEFINITIONS: avConfig.avDefS3KeyPrefix
-      }
-    });
-
-    // grant lambda permission to the S3 bucket
-    s3avdev.grantReadWrite(fnDefDl);
-
-    // add a trigger schedule
-    const schdDefDl = new event.Rule(this, 'definitionsDownloadRule', {
-      description: 'triggers a Lambda function to download the Antivirus definition files',
-      schedule: event.Schedule.rate(cdk.Duration.hours(3)),
-      targets: [
-        new targets.LambdaFunction(fnDefDl)
-      ]
-    });
-/*     eventPattern: {
-      source: ['aws.s3'],
-      resources:[s3avdev.bucketArn],
-      detail: {
-        'eventName': ['PutObject']
-      }
-    } */
-    // #endregion
 
     // create network Resources
     const vpc = new ec2.Vpc(this, 'AvVPC', {
@@ -116,8 +83,62 @@ export class S3AntivirusStack extends cdk.Stack {
       vpc: vpc,
       filesystem: lambda.FileSystem.fromEfsAccessPoint(efsAp, avConfig.mountpoint)
     });
-
+    // grant read permissions to get the definition files
     s3avdev.grantRead(fnMvDev);
+
+    // trigger to move AV definitions to EFS
+/*     const ebRuleMvDevToEfs = new event.Rule(this, 'moveDefinitionsToEfsRule', {
+      description: 'triggers a Lambda to move the Defintions from S3 to EFS',
+      eventPattern: {
+        source: ['aws.lambda'],
+        detail: {
+          eventName: ['Invoke'],
+          resources: [
+            {
+              ARN: fnDefDl.functionArn
+            }
+          ]
+        }
+      }
+    }); */
+
     // #endregion
+
+    // #region - Download AV definitions from the web
+
+    // Lambda function that downloads definition files
+    const fnDefDl = new lambda.Function(this, 'definitionsDownload', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'download-definitions.lambdaHandleEvent',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda', 'clamav')),
+      memorySize: 1024,
+      timeout: cdk.Duration.minutes(2),
+      environment: {
+        CLAMAV_BUCKET_NAME: s3avdev.bucketName,
+        PATH_TO_AV_DEFINITIONS: avConfig.avDefS3KeyPrefix
+      },
+      onSuccess: new destinations.LambdaDestination(fnMvDev)
+    });
+
+    // grant lambda permission to the S3 bucket for AV definitions
+    s3avdev.grantReadWrite(fnDefDl);
+
+    // add a trigger schedule
+    const ebRuleDefDl = new event.Rule(this, 'definitionsDownloadRule', {
+      description: 'triggers a Lambda function to download the Antivirus definition files',
+      schedule: event.Schedule.rate(cdk.Duration.hours(3)),
+      targets: [
+        new targets.LambdaFunction(fnDefDl)
+      ]
+    });
+/*     eventPattern: {
+      source: ['aws.s3'],
+      resources:[s3avdev.bucketArn],
+      detail: {
+        'eventName': ['PutObject']
+      }
+    } */
+    // #endregion
+
   }
 }
