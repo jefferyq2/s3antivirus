aa67be82cdb2c2d7d6830913ee4f3c81009c2f17
Andy Brunner <andy@cloudxs.ch>
2021-03-26 08:19:41 +0100

    some ESLint cleanup

--- bin/s3antivirus.ts
+++ bin/s3antivirus.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { S3AntivirusStack } from '../lib/s3antivirus-stack';
-import { BuildConfig }  from '../lib/build-config';
+import { BuildConfig } from '../lib/build-config';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Tags } from '@aws-cdk/core';
@@ -10,53 +10,45 @@ const yaml = require('js-yaml');
 
 const app = new cdk.App();
 
-
-
-function ensureString(object: { [name: string]: any }, propName: string ): string
-{
-    if(!object[propName] || object[propName].trim().length === 0)
-        throw new Error(propName +" does not exist or is empty");
-
-    return object[propName];
+function ensureString (object: { [name: string]: any }, propName: string): string {
+  if (!object[propName] || object[propName].trim().length === 0) { throw new Error(propName + ' does not exist or is empty'); }
+  return object[propName];
 }
 
-function getConfig() {
-  let env = app.node.tryGetContext('config');
+function getConfig () {
+  const env = app.node.tryGetContext('config');
   if (!env) {
     throw new Error('Context variable missing on CDK command. Pass in as \'-c config=xxx\'');
   }
 
-  let unparsedEnv = yaml.load(fs.readFileSync(path.resolve("./config/"+env+".yaml"), "utf8"));
+  const unparsedEnv = yaml.load(fs.readFileSync(path.resolve('./config/' + env + '.yaml'), 'utf8'));
 
-  let buildConfig: BuildConfig = {
-      AWSAccountID: ensureString(unparsedEnv, 'AWSAccountID'),
-      AWSProfileName: ensureString(unparsedEnv, 'AWSProfileName'),
-      AWSProfileRegion: ensureString(unparsedEnv, 'AWSProfileRegion'),
+  const buildConfig: BuildConfig = {
+    AWSAccountID: ensureString(unparsedEnv, 'AWSAccountID'),
+    AWSProfileName: ensureString(unparsedEnv, 'AWSProfileName'),
+    AWSProfileRegion: ensureString(unparsedEnv, 'AWSProfileRegion'),
 
-      App: ensureString(unparsedEnv, 'App'),
-      Version: ensureString(unparsedEnv, 'Version'),
-      Environment: ensureString(unparsedEnv, 'Environment')
+    App: ensureString(unparsedEnv, 'App'),
+    Version: ensureString(unparsedEnv, 'Version'),
+    Environment: ensureString(unparsedEnv, 'Environment')
   };
 
   return buildConfig;
 }
 
+function Main () {
+  const buildConfig: BuildConfig = getConfig();
 
-
-function Main() {
-
-  let buildConfig: BuildConfig = getConfig();
-  
+  // set Tags for the whole app
   Tags.of(app).add('App', buildConfig.App);
   Tags.of(app).add('Environment', buildConfig.Environment);
-  
-  let s3AntivirusStackName = buildConfig.App + "-" + buildConfig.Environment + "-main";
+
+  const s3AntivirusStackName = buildConfig.App + '-' + buildConfig.Environment + '-main';
   const mainStack = new S3AntivirusStack(app, s3AntivirusStackName, {
     env: {
-        region: buildConfig.AWSProfileRegion,
-        account: buildConfig.AWSAccountID
+      region: buildConfig.AWSProfileRegion,
+      account: buildConfig.AWSAccountID
     }
   });
-
 }
 Main();
